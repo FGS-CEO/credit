3bf8e2ddd9d48d443d98494dbb363e2fd49257f8
FGS-CEO <164756267+FGS-CEO@users.noreply.github.com>
2024-03-24 19:19:58 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,32 @@
-let members = [
-    { name: "Dio", gfs: 1000 },
+ 
+const members = [
     { name: "Insane", gfs: 0 },
     { name: "Jay", gfs: 0 },
     { name: "Hakuna", gfs: 0 },
     { name: "LoneWolf", gfs: 0},
     { name: "Denp", gfs: 0 },
     { name: "Pan", gfs: 0},
+    { name: "Dio", gfs: 1000 },
+    {name: "Null", gfs: 100},
 ];
 
-const tableBody = document.querySelector(".table-body");
 
-let orderedMembers = members.sort((a,b) => a.gfs > b.gfs ? -1 : +1);
-orderedMembers.forEach((a, index)=> {
-    const tr = document.createElement("tr");
-    const rank = document.createElement("th");
-    rank.textContent= (index + 1);
-    tr.appendChild(rank);
+const TableBody = document.querySelector(".table-body");
 
-    const name = document.createElement("th");
-    name.textContent= a.name;
-    tr.appendChild(name);
 
-    const credit = document.createElement("td");
-    credit.textContent= a.gfs;
-    tr.appendChild(credit);
+let tableHtml = '';
 
-    const relevancy = document.createElement("td");
-    if (a.gfs > 0){
-        relevancy.textContent = "Relevant";
-    } else if ( a.gfs < 0){
-        relevancy.textContent = "Irrelevant";
-    } else {
-        relevancy.textContent = "-"
-    }
-    tr.appendChild(relevancy);
+// Loop through the members after sorting them to build the table body.
+members.sort((a,b) => a.gfs > b.gfs ? -1 : +1).forEach((a, index) => {
+    tableHtml += `
+        <tr>
+            <th>${index + 1}</th>
+            <th>${a.name}</th>
+            <td>${a.gfs}</td>
+            <td>${a.gfs > 0 ? "Relevant": a.gfs < 0 ? "Irrelevant" : "-"}</td>
+        <tr/>
+    `
+});
 
-    tableBody.appendChild(tr);
-})
+// Update the DOM.
+TableBody.innerHTML = tableHtml;
